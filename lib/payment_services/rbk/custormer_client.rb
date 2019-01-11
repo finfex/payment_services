8b058cb9549441f0a1e501bb3640cefac46771e5
Alexey Naumov <alexsnaumov@gmail.com>
2019-01-11 13:29:14 +0300

    [#162634176] Возврат оплаты для РБК WIP

---
+++ lib/payment_services/rbk/custormer_client.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2018 FINFEX https://github.com/finfex
+
+class PaymentServices::RBK
+  class CustomerClient < PaymentServices::RBK::Client
+    def create_customer(user)
+      request_body = {
+        shopID: SHOP,
+        contactInfo: {
+          email: user.email,
+          phone: user.phone
+        },
+        metadata: { user_id: user.id }
+      }
+      safely_parse http_request(
+        url: CUSTOMERS_URL,
+        method: :POST,
+        body: request_body
+      )
+    end
+
+    def customer_status(customer)
+      safely_parse http_request(
+        url: "#{CUSTOMERS_URL}/#{customer.rbk_id}",
+        method: :GET
+      )
+    end
+
+    def customer_events(customer)
+      safely_parse http_request(
+        url: "#{CUSTOMERS_URL}/#{customer.rbk_id}/events?limit=100",
+        method: :GET
+      )
+    end
+
+    def customer_bindings(customer)
+      safely_parse http_request(
+        url: "#{CUSTOMERS_URL}/#{customer.rbk_id}/bindings",
+        method: :GET
+      )
+    end
+
+    def get_token(customer)
+      safely_parse http_request(
+        url: "#{CUSTOMERS_URL}/#{customer.rbk_id}/access-tokens",
+        method: :POST
+      )
+    end
+  end
+end
---
+++ lib/payment_services/rbk/invoice_client.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2018 FINFEX https://github.com/finfex
+
+require_relative 'client'
+
+class PaymentServices::RBK
+  class InvoiceClient < PaymentServices::RBK::Client
+    def create_invoice(order_id:, amount:)
+      request_body = {
+        shopID: SHOP,
+        dueDate: Time.zone.now + MAX_INVOICE_LIVE,
+        amount: amount,
+        currency: DEFAULT_CURRENCY,
+        product: I18n.t('payment_systems.default_product', order_id: order_id),
+        metadata: { order_public_id: order_id }
+      }
+      safely_parse http_request(
+        url: INVOICES_URL,
+        method: :POST,
+        body: request_body
+      )
+    end
+
+    def pay_invoice_by_customer(invoice:, customer:)
+      request_body = {
+        flow: { type: 'PaymentFlowInstant' },
+        payer: {
+          payerType: 'CustomerPayer',
+          customerID: customer.rbk_id
+        }
+      }
+      safely_parse http_request(
+        url: "#{INVOICES_URL}/#{invoice.rbk_invoice_id}/payments",
+        method: :POST,
+        body: request_body,
+        headers: { Authorization: "Bearer #{invoice.access_payment_token}" }
+      )
+    end
+  end
+end
