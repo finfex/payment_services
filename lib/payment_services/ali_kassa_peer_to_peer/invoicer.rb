7f295ba3f6546121935f41965fb1dbc84bb0ed07
Alexey Naumov <alexsnaumov@gmail.com>
2020-04-02 16:56:41 +0300

    AliKassaPeerToPeer card number

--- lib/payment_services/ali_kassa_peer_to_peer/invoicer.rb
+++ lib/payment_services/ali_kassa_peer_to_peer/invoicer.rb
@@ -8,14 +8,14 @@ class PaymentServices::AliKassaPeerToPeer
   class Invoicer < ::PaymentServices::Base::Invoicer
     ALIKASSA_PAYMENT_FORM_URL = 'https://sci.alikassa.com/payment'
     ALIKASSA_RUB_CURRENCY = 'RUB'
-    ALIKASSA_CARD = 'Card'
+    ALIKASSA_CARD = 'сard'
 
     def create_invoice(money)
       Invoice.create!(amount: money, order_public_id: order.public_id)
     end
 
     def invoice_form_data
-      {
+      invoice_data = {
         url: ALIKASSA_PAYMENT_FORM_URL,
         method: 'POST',
         target: '_blank',
@@ -25,11 +25,13 @@ class PaymentServices::AliKassaPeerToPeer
           orderId: order.public_id,
           amount: order.invoice_money.to_f,
           currency: ALIKASSA_RUB_CURRENCY,
-          payWayVia: ALIKASSA_CARD,
+          payWayVia: order.income_payment_system.payway&.capitalize,
           desc: I18n.t('payment_systems.default_product', order_id: order.public_id),
           customerEmail: order.user.try(:email)
         }
       }
+      invoice_data[:inputs][:number] = order.income_account.gsub(/\D/, '') if order.income_payment_system.payway == ALIKASSA_CARD
+      invoice_data
     end
   end
 end
