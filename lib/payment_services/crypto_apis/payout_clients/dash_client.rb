69e82532e21eab6a2128865747478bf4c12d2c3a
Roman <roman0tershak@gmail.com>
2021-06-23 11:39:21 +0300

    Cryptoapis/Dash: Fix payouts (#41)

--- lib/payment_services/crypto_apis/payout_clients/dash_client.rb
+++ lib/payment_services/crypto_apis/payout_clients/dash_client.rb
@@ -5,6 +5,16 @@ require_relative 'base_client'
 class PaymentServices::CryptoApis
   module PayoutClients
     class DashClient < PaymentServices::CryptoApis::PayoutClients::BaseClient
+      DEPRECATED_OPTION = { deprecated_rpc: 'sign_raw_transaction' }
+
+      def make_payout(payout:, wallet:)
+        safely_parse http_request(
+          url: "#{base_url}/txs/new",
+          method: :POST,
+          body: api_query_for(payout, wallet).merge(DEPRECATED_OPTION)
+        )
+      end
+
       private
 
       def base_url
