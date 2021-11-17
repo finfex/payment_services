8532801edb03754b972ba6eb235ff6c2764eaa54
Roman <roman0tershak@gmail.com>
2021-11-17 16:31:48 +0200

    Cryptoapis: match transactions with delta (#71)

--- lib/payment_services/crypto_apis/invoicer.rb
+++ lib/payment_services/crypto_apis/invoicer.rb
@@ -9,6 +9,7 @@ class PaymentServices::CryptoApis
   class Invoicer < ::PaymentServices::Base::Invoicer
     TRANSACTION_TIME_THRESHOLD = 30.minutes
     ETC_TIME_THRESHOLD = 20.seconds
+    PARTNERS_RECEIVED_AMOUNT_DELTA = 0.000001
 
     def create_invoice(money)
       Invoice.create!(amount: money, order_public_id: order.public_id, address: order.income_account_emoney)
@@ -49,18 +50,57 @@ class PaymentServices::CryptoApis
         raise response[:meta][:error][:message] if response.dig(:meta, :error, :message)
 
         response[:payload].find do |transaction|
-          received_amount = transaction[:amount]
-          received_amount = transaction[:received][invoice.address] unless transaction[:received][invoice.address] == invoice.address 
-
-          transaction_created_at = DateTime.strptime(transaction[:timestamp].to_s,'%s').utc
-          invoice_created_at = expected_invoice_created_at
-
-          next if invoice_created_at >= transaction_created_at
-
-          time_diff = (transaction_created_at - invoice_created_at) / 1.minute
-          received_amount&.to_d == invoice.amount.to_d && time_diff.round.minutes < TRANSACTION_TIME_THRESHOLD
+          match_transaction?(transaction)
         end if response[:payload]
       end
+    end
+
+    def match_transaction?(transaction)
+      amount = parse_received_amount(transaction)
+      transaction_created_at = timestamp_in_utc(transaction[:timestamp])
+      invoice_created_at = expected_invoice_created_at
+      return false if invoice_created_at >= transaction_created_at
+
+      time_diff = (transaction_created_at - invoice_created_at) / 1.minute
+      match_by_amount_and_time?(amount, time_diff) || match_by_txid_amount_and_time?(amount, transaction[:txid], time_diff)
+    end
+
+    def match_by_amount_and_time?(amount, time_diff)
+      match_amount?(amount) && match_transaction_time_threshold?(time_diff)
+    end
+    
+    def match_by_txid_amount_and_time?(amount, txid, time_diff)
+      invoice.possible_transaction_id.present? &&
+        match_txid?(txid) &&
+        match_amount_with_delta?(amount) &&
+        match_transaction_time_threshold?(time_diff)
+    end
+
+    def match_amount?(received_amount)
+      received_amount.to_d == invoice.amount.to_d
+    end
+
+    def match_amount_with_delta?(received_amount)
+      amount_diff = received_amount.to_d - invoice.amount.to_d
+      amount_diff >= 0 && amount_diff <= PARTNERS_RECEIVED_AMOUNT_DELTA
+    end
+
+    def match_transaction_time_threshold?(time_diff)
+      time_diff.round.minutes < TRANSACTION_TIME_THRESHOLD
+    end
+
+    def match_txid?(txid)
+      txid == invoice.possible_transaction_id
+    end
+
+    def parse_received_amount(transaction)
+      received_amount = transaction[:amount]
+      received_amount = transaction[:received][invoice.address] unless transaction[:received][invoice.address] == invoice.address 
+      received_amount
+    end
+
+    def timestamp_in_utc(timestamp)
+      DateTime.strptime(timestamp.to_s,'%s').utc
     end
 
     def expected_invoice_created_at
