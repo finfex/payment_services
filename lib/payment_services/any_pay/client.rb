e8138d86e44c8f49c85af7c8662c8d07c029e457
Roman <roman0tershak@gmail.com>
2023-07-08 16:15:47 +0300

    Прием платежей QIWI через шлюз AnyPay (#113)

---
+++ lib/payment_services/any_pay/client.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+class PaymentServices::AnyPay
+  class Client < ::PaymentServices::Base::Client
+    PROJECT_ID = 11555
+    API_URL = "https://anypay.io/api"
+
+    def initialize(api_key:, secret_key:)
+      @api_key    = api_key
+      @secret_key = secret_key
+    end
+
+    def create_invoice(params:)
+      params = { project_id: PROJECT_ID }.merge(params)
+      request_body = params.merge(sign: build_signature(method_name: 'create-payment', params: params))
+      safely_parse(http_request(
+        url: "#{API_URL}/create-payment/#{secret_key}",
+        method: :POST,
+        body: request_body,
+        headers: build_headers
+      )).dig('result')
+    end
+
+    def transaction(deposit_id:)
+      params = { project_id: PROJECT_ID, trans_id: deposit_id }
+      request_body = params.merge(sign: build_signature(method_name: 'payments', params: params))
+      safely_parse(http_request(
+        url: "#{API_URL}/payments/#{secret_key}",
+        method: :POST,
+        body: request_body,
+        headers: build_headers
+      )).dig('result', 'payments', deposit_id)
+    end
+
+    private
+
+    attr_reader :api_key, :secret_key
+
+    def build_headers
+      {
+        'Accept' => 'application/json',
+        'Content-Type' => 'multipart/form-data'
+      }
+    end
+
+    def build_signature(method_name:, params:)
+      sign_string = [
+        method_name, secret_key, params[:project_id], params[:pay_id],
+        params[:amount], params[:currency], params[:desc], params[:method], api_key 
+      ].join
+      Digest::SHA256.hexdigest(sign_string)
+    end
+
+    def build_request(uri:, method:, body: nil, headers: nil)
+      request = if method == :POST
+                  Net::HTTP::Post.new(uri.request_uri, headers)
+                elsif method == :GET
+                  Net::HTTP::Get.new(uri.request_uri, headers)
+                else
+                  raise "Запрос #{method} не поддерживается!"
+                end
+      request.set_form_data(body)
+      request
+    end
+  end
+end
---
+++ lib/payment_services/any_pay/invoicer.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require_relative 'invoice'
+require_relative 'client'
+
+class PaymentServices::AnyPay
+  class Invoicer < ::PaymentServices::Base::Invoicer
+    PAYMENT_METHOD = 'qiwi'
+
+    def create_invoice(money)
+      Invoice.create!(amount: money, order_public_id: order.public_id)
+      response = client.create_invoice(params: invoice_params)
+
+      invoice.update!(
+        deposit_id: response['transaction_id'],
+        pay_url: response['payment_url']
+      )
+    end
+
+    def pay_invoice_url
+      invoice.present? ? URI.parse(invoice.reload.pay_url) : ''
+    end
+
+    def async_invoice_state_updater?
+      true
+    end
+
+    def update_invoice_state!
+      transaction = client.transaction(deposit_id: invoice.deposit_id)
+      invoice.update_state_by_provider(transaction['status']) if transaction
+    end
+
+    def invoice
+      @invoice ||= Invoice.find_by(order_public_id: order.public_id)
+    end
+
+    private
+
+    delegate :income_payment_system, to: :order
+    delegate :currency, to: :income_payment_system
+
+    def invoice_params
+      {
+        pay_id: order.public_id.to_s,
+        amount: invoice.amount.to_f,
+        currency: currency.to_s,
+        desc: order.public_id.to_s,
+        method: PAYMENT_METHOD,
+        email: order.user_email,
+        success_url: order.success_redirect,
+        fail_url: order.failed_redirect
+      }
+    end
+
+    def client
+      @client ||= Client.new(api_key: api_key, secret_key: api_secret)
+    end
+  end
+end
